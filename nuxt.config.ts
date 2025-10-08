3293446347d931be325baa38fa7a430b36f08d5c
Alex Russell <slightlyoff@gmail.com>
2025-10-08 01:58:34 -0700

    add PWA manifest and SW

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
+let sw = false;
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n", "@nuxt/icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/i18n",
+    "@nuxt/icon",
+    "@vite-pwa/nuxt",
+  ],
   devtools: { enabled: true },
   app: {
     head: {
@@ -58,7 +65,67 @@ export default defineNuxtConfig({
   },
   nitro: {
     prerender: {
+      routes: [ "/" ],
       autoSubfolderIndex: false,
     },
   },
+  pwa: {
+    enabled: true,
+    strategies: 'generateSW',
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'LocalSend Web',
+      short_name: 'LocalSend',
+      theme_color: '#111827',
+      background_color: '#111827',
+      scope: "/",
+      id: "localsend",
+      start_url: "/?pwa=1",
+      icons: [
+        {
+          src: 'apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png',
+        },
+        {
+          src: 'logo-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: [
+        '/',
+        '**/*.{js,css,html,png,svg,ico}',
+      ],
+      navigateFallback: '/',
+      runtimeCaching: [{
+        urlPattern: /^https:\/\/api\.iconify\.design\/.*'/i,
+        handler: "CacheFirst",
+        options:  {
+          cacheName: "icons",
+          expiration: {
+            maxEntries: 10,
+          },
+        }
+      }],
+    },
+    // generateSW: {
+    //   globPatterns: [
+    //     '**/*.{js,css,html,png,svg,ico}',
+    //   ],
+    // },
+    client: {
+      installPrompt: true,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  }
 });
